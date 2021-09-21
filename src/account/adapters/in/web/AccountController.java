862c1ec8c61391effd4bc34d59c40ab53d7710e2
Bruno Alliguieri <engenharia1@azship.com.br>
2021-09-21 17:15:20 -0300

    account controller created

--- src/account/adapters/in/web/AccountController.java
+++ src/account/adapters/in/web/AccountController.java
@@ -1,5 +1,16 @@
 package account.adapters.in.web;
 
+import account.application.port.in.SendMoneyUseCase;
+import account.domain.Account;
+
 public class AccountController {
-    
+    private SendMoneyUseCase sendMoneyUseCase;
+
+    public AccountController(SendMoneyUseCase sendMoneyUseCase) {
+        this.sendMoneyUseCase = sendMoneyUseCase;
+    }
+
+    public void sendMoneyHandler(long accountId, Account updatedAccount) {
+        sendMoneyUseCase.sendMoney(accountId, updatedAccount);
+    }
 }

d27af52949a60761a9b077257de863799c0d48d4
Bruno Alliguieri <alliguierisilva@hotmail.com>
2021-09-21 23:30:23 -0300

    interface implementation and method removed

--- src/account/adapters/out/AccountPersistanceAdapter.java
+++ src/account/adapters/out/AccountPersistanceAdapter.java
@@ -1,17 +1,12 @@
 package account.adapters.out;
 
 import account.application.port.out.LoadAccountPort;
-import account.application.port.out.UpdateAccountPort;
 import account.domain.Account;
 
-public class AccountPersistanceAdapter implements LoadAccountPort, UpdateAccountPort {
+public class AccountPersistanceAdapter implements LoadAccountPort {
     
     public Account loadAccount(long accountId) {
         return new Account(100);
-    }
-
-    public void updateAccount(Account oldAccount, Account updatedAccount) {
-        oldAccount.update(updatedAccount);
     }
 
 }

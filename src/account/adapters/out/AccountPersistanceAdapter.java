6bdd91b32674b51b92b0d9d27ad569df96125bde
Bruno Alliguieri <engenharia1@azship.com.br>
2021-09-22 16:29:20 -0300

    update account method added to persistance adapter

--- src/account/adapters/out/AccountPersistanceAdapter.java
+++ src/account/adapters/out/AccountPersistanceAdapter.java
@@ -1,12 +1,17 @@
 package account.adapters.out;
 
 import account.application.port.out.LoadAccountPort;
+import account.application.port.out.UpdateAccountPort;
 import account.domain.Account;
 
-public class AccountPersistanceAdapter implements LoadAccountPort {
+public class AccountPersistanceAdapter implements LoadAccountPort, UpdateAccountPort {
     
     public Account loadAccount(long accountId) {
         return new Account(100);
+    }
+
+    public void updateAccount(Account oldAccount, Account newAccount) {
+        newAccount.withdraw(50, newAccount);
     }
 
 }

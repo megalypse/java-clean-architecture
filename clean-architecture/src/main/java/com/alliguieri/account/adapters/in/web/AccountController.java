5647c3c4028652c157c93c879bca9a16b5a74531
Bruno Alliguieri <engenharia1@azship.com.br>
2021-09-23 11:41:59 -0300

    AccountController implemented BaseControllerInterface

--- clean-architecture/src/main/java/com/alliguieri/account/adapters/in/web/AccountController.java
+++ clean-architecture/src/main/java/com/alliguieri/account/adapters/in/web/AccountController.java
@@ -2,8 +2,9 @@ package com.alliguieri.account.adapters.in.web;
 
 import com.alliguieri.account.application.port.in.SendMoneyUseCase;
 import com.alliguieri.account.domain.Account;
+import com.alliguieri.common.contracts.BaseController;
 
-public class AccountController {
+public class AccountController implements BaseController {
     private SendMoneyUseCase sendMoneyUseCase;
 
     public AccountController(SendMoneyUseCase sendMoneyUseCase) {
@@ -12,5 +13,15 @@ public class AccountController {
 
     public void sendMoneyHandler(long accountId, Account updatedAccount) {
         sendMoneyUseCase.sendMoney(accountId, updatedAccount);
+    }
+
+    public void handleContext(String context, Object... data) {
+        switch(context) {
+            case "/send-money":
+                sendMoneyHandler((long)data[0], (Account)data[1]);
+                break;
+            default:
+                throw new RuntimeException("Context not found");
+        }
     }
 }

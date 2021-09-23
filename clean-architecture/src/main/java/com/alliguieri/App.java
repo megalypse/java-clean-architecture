44a3b8588d362b9b3cfb25343aa83cb19c4f608f
Bruno Alliguieri <engenharia1@azship.com.br>
2021-09-23 11:42:41 -0300

    application assembly done

--- clean-architecture/src/main/java/com/alliguieri/App.java
+++ clean-architecture/src/main/java/com/alliguieri/App.java
@@ -1,5 +1,12 @@
 package com.alliguieri;
 
+import java.util.List;
+
+import com.alliguieri.account.adapters.in.web.AccountController;
+import com.alliguieri.account.adapters.out.AccountPersistanceAdapter;
+import com.alliguieri.account.application.SendMoneyService;
+import com.alliguieri.account.domain.Account;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        var accountPersistanceAdapter = new AccountPersistanceAdapter();
+        var sendMoneyService = new SendMoneyService(accountPersistanceAdapter, accountPersistanceAdapter);
+        var accountController = new AccountController(sendMoneyService);
+        var router = new Router(accountController);
+
+        // on request
+        router.handleRoute("/account", 1, new Account(2, 50));
     }
 }
